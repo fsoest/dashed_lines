af091b67c04d0f0e5a91304f324598ee599d264c
fsoest <46381886+fsoest@users.noreply.github.com>
2022-07-24 01:55:53 +0200

    Update dashed_lines.py

--- dashed_lines.py
+++ dashed_lines.py
@@ -1,6 +1,5 @@
 from base import dec_to_dms, dms_to_dec
 import numpy as np
-from mpl_toolkits.basemap import Basemap
 from argparse import ArgumentParser
 
 
@@ -9,12 +8,8 @@ def main(lat1, lon1, lat2, lon2, n, txt):
     lon1_dec = dms_to_dec(lon1)
     lat2_dec = dms_to_dec(lat2)
     lon2_dec = dms_to_dec(lon2)
-
-    def bm(a, b, inverse=True):
-        return a, b
-
-    x1, y1 = bm(lon1_dec, lat1_dec)
-    x2, y2 = bm(lon2_dec, lat2_dec)
+    x1, y1 = lon1_dec, lat1_dec
+    x2, y2 = lon2_dec, lat2_dec
     c1 = np.array([x1, y1])
     c2 = np.array([x2, y2])
     delta = (c2 - c1) / (2 * n)
@@ -22,8 +17,8 @@ def main(lat1, lon1, lat2, lon2, n, txt):
     for i in range(n):
         lower = c1 + 2 * i * delta
         upper = c1 + (2 * i + 1) * delta
-        l1, l2 = bm(lower[0], lower[1], inverse=True)
-        u1, u2 = bm(upper[0], upper[1], inverse=True)
+        l1, l2 = lower[0], lower[1]
+        u1, u2 = upper[0], upper[1]
         print(dec_to_dms(l2), dec_to_dms(l1), dec_to_dms(u2), dec_to_dms(u1), txt)
 
 
